6c9db41f6036b036a9449b2af1badf255ff89ad8
ananddtyagi <anand.deep.tyagi@gmail.com>
2024-12-30 02:13:28 -0500

    save transcript

--- renderer/types/electron.d.ts
+++ renderer/types/electron.d.ts
@@ -4,6 +4,8 @@ export interface ElectronAPI {
     createNote: (content?: string) => Promise<string>;
     updateNote: (uuid: string, content: string) => Promise<boolean>;
     transcribeAudio: (base64Audio: string) => Promise<string | null>;
+    addToTranscription: (note_uuid: string, text: string) => Promise<string>;
+    getTranscription: (note_uuid: string) => Promise<string>;
 }
 
 declare global {
